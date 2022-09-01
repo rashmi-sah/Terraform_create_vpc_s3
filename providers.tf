f93faa799c215c1aeb548e0c920d904acb6f3698
rashmi sah <105689930+rashmi-sah@users.noreply.github.com>
2022-09-01 17:59:42 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,10 +1,3 @@
-terraform {
-  required_version = ">0.14.7"
-
-  required_providers {
-    aws = {
-      version = ">= 3.66.0"
-      source  = "hashicorp/aws"
-    }
-  }
-}
+provider "aws" {
+   region = "us-east-1"
+ }
