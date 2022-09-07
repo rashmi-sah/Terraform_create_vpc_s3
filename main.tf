6c5586068be3f0e5b73fdd09afc46f2e4205ea61
rashmi sah <105689930+rashmi-sah@users.noreply.github.com>
2022-09-07 12:26:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
+/*
 resource "aws_vpc" "vpc1" {
   cidr_block = "10.1.0.0/16"
   instance_tenancy = "default"
+}
+*/
+  
+resource "aws_s3control_bucket" "example" {
+  bucket     = "example"
+  outpost_id = data.aws_outposts_outpost.example.id
 }
